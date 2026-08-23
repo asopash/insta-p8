c1bcf67d35bd1222593cc834ba164bef9389cc15
asopash <77784409+asopash@users.noreply.github.com>
2026-08-23 14:46:01 +0330

    Update route.ts

--- app/api/zernio/callback/route.ts
+++ app/api/zernio/callback/route.ts
@@ -2,10 +2,13 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
 
-  const connected = req.nextUrl.searchParams.get("connected")
-  const profileId = req.nextUrl.searchParams.get("profileId")
-  const accountId = req.nextUrl.searchParams.get("accountId")
-  const username = req.nextUrl.searchParams.get("username")
+  const params = req.nextUrl.searchParams
+
+  const connected = params.get("connected")
+  const profileId = params.get("profileId")
+  const accountId = params.get("accountId")
+  const username = params.get("username")
+
 
   console.log("ZERNIO CALLBACK", {
     connected,
@@ -15,7 +18,43 @@ export async function GET(req: NextRequest) {
   })
 
 
-  return NextResponse.redirect(
-  "https://insta-p8.up.railway.app/dashboard"
-)
+  if (!profileId || !accountId) {
+    return NextResponse.json(
+      {
+        error: "Missing zernio data",
+        profileId,
+        accountId
+      },
+      {
+        status:400
+      }
+    )
+  }
+
+
+  const response = NextResponse.redirect(
+    new URL("/dashboard", req.url)
+  )
+
+
+  // ذخیره اتصال کاربر
+  response.cookies.set(
+    "zernio_session",
+    JSON.stringify({
+      profileId,
+      accountId,
+      username,
+      platform: connected
+    }),
+    {
+      httpOnly:true,
+      secure:true,
+      sameSite:"lax",
+      path:"/",
+      maxAge:60*60*24*30
+    }
+  )
+
+
+  return response
 }
