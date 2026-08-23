54cb900e6b2335bcc6d1ba1743c160696f2048d4
asopash <77784409+asopash@users.noreply.github.com>
2026-08-23 11:25:58 +0330

    Update route.ts

--- app/api/zernio/connect/route.ts
+++ app/api/zernio/connect/route.ts
@@ -1,15 +1,32 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
-export async function GET(request: NextRequest) {
+export async function GET() {
+  try {
+    const response = await fetch(
+      `https://zernio.com/api/v1/connect/instagram?profileId=${process.env.ZERNIO_PROFILE_ID}&redirect_url=${encodeURIComponent(
+        "https://insta-p8.up.railway.app/api/zernio/callback"
+      )}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.ZERNIO_API_KEY}`,
+        },
+      }
+    )
 
-  const zernioUrl = process.env.ZERNIO_CONNECT_URL
+    const data = await response.json()
 
-  if (!zernioUrl) {
+    console.log("ZERNIO CONNECT:", data)
+
+    if (!data.authUrl) {
+      return NextResponse.json(data, { status: 400 })
+    }
+
+    return NextResponse.redirect(data.authUrl)
+
+  } catch (error:any) {
     return NextResponse.json(
-      { error: "Missing ZERNIO_CONNECT_URL" },
+      { error: error.message },
       { status: 500 }
     )
   }
-
-  return NextResponse.redirect(zernioUrl)
 }
